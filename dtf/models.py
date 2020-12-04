4010a9612b04e2364f61dcca7ff20872034192d8
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2020-12-04 17:59:48 +0100

    Introduce "reference sets" to model

--- dtf/models.py
+++ dtf/models.py
@@ -1,6 +1,9 @@
 """
 Module containing all database definitions
 """
+import collections
+import json
+
 from django.db import models
 
 # Create your models here.
@@ -60,7 +63,7 @@ class Project(models.Model):
         app_label = 'dtf'
 
 class ProjectSubmissionProperty(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name="properties")
 
     name = models.CharField(max_length=100, blank=False)
     required = models.BooleanField(default=False)
@@ -84,7 +87,7 @@ class Submission(models.Model):
 
     Basically a submission is a run of a whole test suite. Every test and parameter of that suit gets assigned to this submission
     """
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name="submissions")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     info = models.JSONField(null=False, default=dict)
@@ -151,6 +154,28 @@ class TestResult(models.Model):
     class Meta:
         app_label = 'dtf'
 
+
+class ReferenceSet(models.Model):
+
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name="reference_sets")
+
+    # We do use a special JSON decoder, that creates `OrderedDict` instead of `dict`
+    # objects. This will allow us to sort the properties by their keys, so that the
+    # ordering of entries does not mess with the `unique` database field constraint.
+    class OrderedDictJSONDecoder(json.JSONDecoder):
+        def __init__(self, *args, **kwargs):
+            kwargs['object_pairs_hook']=collections.OrderedDict
+            super().__init__(*args, **kwargs)
+    property_values = models.JSONField(decoder=OrderedDictJSONDecoder, null=False, default=collections.OrderedDict, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not isinstance(self.property_values, collections.OrderedDict):
+            self.property_values = collections.OrderedDict(sorted(self.property_values.items()))
+        super().save(*args, **kwargs)
+
+    class Meta:
+        app_label = 'dtf'
+
 class TestReference(models.Model):
     """
     Model to store references to every test
@@ -159,7 +184,7 @@ class TestReference(models.Model):
     The test_name is not, since the references can be from different test result \
         objects. The test name will be the same though. The test_name must not be UNIQUE constraint though, in order to allow equally named tests from multiple projects to be saved
     """
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+    reference_set = models.ForeignKey(ReferenceSet, on_delete=models.SET_NULL, null=True, related_name="test_references")
     test_name = models.CharField(max_length=100, blank=False)
     # maybe this should just have a testresult as a foreign key?
     references = models.JSONField(null=False, default=dict)
