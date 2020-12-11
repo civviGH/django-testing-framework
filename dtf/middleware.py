38aa2feae8b5e978bb886892895dc8a2fc0e9aea
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2020-12-11 09:27:59 +0100

    Add a setting for the default display timezone

--- dtf/middleware.py
+++ dtf/middleware.py
@@ -1,10 +1,12 @@
 
 from django.utils import timezone
+from django.conf import settings
 
 class TimezoneMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        timezone.activate("Europe/Berlin") # TODO: get user timezone
+        # TODO: get timezone from user profile when users have been implemented.
+        timezone.activate(settings.DTF_DEFAULT_DISPLAY_TIME_ZONE)
         return self.get_response(request)
