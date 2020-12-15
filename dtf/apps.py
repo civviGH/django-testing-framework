2a7cd5acd1cd907e8120155575e2b61c372e89f6
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2020-12-15 11:31:47 +0100

    Add initial webhooks implementation

--- dtf/apps.py
+++ dtf/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class DtfConfig(AppConfig):
     name = 'dtf'
     verbose_name = 'Django Testing Framework'
+
+    def ready(self):
+        from .webhooks import connect_webhook_signals
+        connect_webhook_signals()
